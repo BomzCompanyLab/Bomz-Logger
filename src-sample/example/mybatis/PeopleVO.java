afc5ec0bc679a2db910f54e2de68d10f83ac5507
bom bom <bom@bomz.app>
2021-09-26 05:22:21 +0000

    ver 1.1

---
+++ src-sample/example/mybatis/PeopleVO.java
@@ -0,0 +1,35 @@
+package example.mybatis;
+
+public class PeopleVO {
+	private String name;
+	private int age;
+	private String addr;
+	
+	public PeopleVO(){}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+	
+}
